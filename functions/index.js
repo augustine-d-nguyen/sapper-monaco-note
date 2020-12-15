4b8eeff986c46340ec33a87013ec9f7ec72239a9
augustine d. nguyen <augustine.d.nguyen@outlook.com>
2020-12-15 22:41:08 +0700

    7. point http request from firebase function to beastmaster app

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
+const { beastmaster } = require('./__sapper__/build/server/server');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.app = functions.https.onRequest((request, response) => {
+  // - Remove duplicated baseUrl.
+  // - In case url rewriting by firebase hosting.
+  request.url = request.url.replace(request.baseUrl.toString(), '');
+  // - Send to beastmaster to handle.
+  beastmaster(request, response);
+});
